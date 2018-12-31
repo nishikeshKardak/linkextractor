e4021c66f9094ba057d5cb38407718114bec9e00
Nishikesh Kardak <39300717+nishikeshKardak@users.noreply.github.com>
2018-12-31 11:42:51 +0530

    Solve issues

--- extractor.py
+++ extractor.py
@@ -2,20 +2,20 @@ import requests
 import bs4
 import urllib
 
-link = input("Enter the url you want to extract")
+web_page = input("Enter the url or link of webpage that you wish to extract")
 
-res = requests.get(link)
+res = requests.get('web_page')
 page = res.text
 
 #function for taking next target file
 def get_next_target(page):
-    start_link = page.find('>href=')
+    start_link = page.find('<a href=')
     if start_link == -1:
         return None , 0
     #print(start_link)
-    start_quote = page.find('"',start_link)
+    start_quote = page.find('"', start_link)
     #print(start_quote)
-    end_quote = page.find('"',start_quote+1)
+    end_quote = page.find('"', start_quote+1)
     #print(end_quote)
     url = page[start_quote + 1:end_quote]
     return url, end_quote
@@ -23,12 +23,18 @@ def get_next_target(page):
 
 #print all links that extract from page
 def print_all_links(page):
+    #links = []
     while True:
         url, endpos = get_next_target(page)
         if url:
-            print (url)
+            #links.append(url)
+            print(url)
             page = page[endpos:]
         else:
             break
+    #return links
 
 print_all_links(page)
+
+
+
